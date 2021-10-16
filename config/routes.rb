415535a1725ce3eb6affbfad9089e386b29e6fb2
Ubuntu <ubuntu@ip-172-31-20-111.us-east-2.compute.internal>
2021-10-16 02:46:30 +0000

    scaffolding for Product, plus routes.db idea to support wrapper around fakeStore products call

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
+# from generate scaffold products
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+    resources :products
 end
+
+# TO DO: replace above & implement to add fakestore handling
+# include creation of FakestoreControler, etc.
+# Rails.application.routes.draw do
+#     resources :products do
+#         resources :fake_store
+#     end
+# end
+
