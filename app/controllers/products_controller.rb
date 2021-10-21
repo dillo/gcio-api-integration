7f8244a7c52fae38b4c2418a8af174a12ea7e636
tekniqul <harold@fortuitous-consulting.biz>
2021-10-20 20:13:55 -0400

    method signature for price range processing

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,6 +5,8 @@ class ProductsController < ActionController::API
 
   @@fake_store_param = "fake_store"
   @@fake_store_products_URI = "https://fakestoreapi.com/products/"
+
+  @filtered_by_category = false
   
   def self.get_fake_store_param
     @@fake_store_param
@@ -14,30 +16,41 @@ class ProductsController < ActionController::API
     @@fake_store_products_URI
   end
   
-     # GET /products
+  # GET /products
+  # /products?fake_store
+  # /products?fake_store&category=electronics
+  # /products?fake_store&category=electronics&price=100,200
   def index
-      if request.parameters.has_key? @@fake_store_param
+    if request.parameters.has_key? @@fake_store_param
 
-        # OK to redirect IF no category or price in the request.parameters
-        # note the changed handling will affect the test code; 
-        # new logic w/rest-client means we get HTTP 204 No Content back
-        # redirect_to route_to_fake_store request.parameters
+      # OK to redirect IF no category or price in the request.parameters
+      # note the changed handling will affect the test code; 
+      # new logic w/rest-client means we get HTTP 204 No Content back
+      # redirect_to route_to_fake_store request.parameters
 
-        fake_store_products = JSON.parse( RestClient.get route_to_fake_store request.parameters )
-        render json: fake_store_products
-      else
-        @products = Product.all
-        render json: @products
+      fake_store_products = JSON.parse( RestClient.get route_to_fake_store request.parameters )
+
+      # if we filter by price, we must also be passing a category per requirements
+      # price=100,200
+      if @filtered_by_category && (request.parameters.has_key? "price" )
+        price_value = request.parameters[:price]
+        fake_store_products =  filter_on_price_range fake_store_products, price_value
       end
+
+      render json: fake_store_products
+    else
+      @products = Product.all
+      render json: @products
     end
+  end
     
   # return fake_store URI based on request parameters
   def route_to_fake_store request_params
         fake_store_url = @@fake_store_products_URI
-        # get the category name-value pair
         category_value = ""
         category_URI_append = ""
         if request_params.has_key? "category"
+          @filtered_by_category = true
           category_value += request_params[:category]
           # some categories are men's clothing, women's clothing
           category_value = URI.escape category_value
@@ -46,6 +59,15 @@ class ProductsController < ActionController::API
           fake_store_url += category_URI_append
         end
           fake_store_url
+  end
+
+  # input is json from fakestoreapi products, price range string
+  # possibly already filtered by category
+  # return altered json
+  def  filter_on_price_range product_json, price_range
+      puts "the price range is " + price_range
+      # FILL IN LOGIC
+      product_json
   end
 
   # GET /products/1
@@ -95,3 +117,4 @@ class ProductsController < ActionController::API
       params.require(:product).permit(:title, :price, :category, :description, :image)
     end
 end
+
